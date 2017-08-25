a0dd3edad493d596868881bbf814f8fd3be7ef06
YQ <820427564@qq.com>
2017-08-26 00:02:55 +0800

    删除ssh爆破模块冗余代码。

--- compoment/ssh_burp.py
+++ compoment/ssh_burp.py
@@ -8,23 +8,18 @@ class ssh_burp(burp.burp):
         if self.stop_signal:
             return None
         try:
-            burp=True
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            try:
-                ssh.connect(self.target,self.port,self.now_user,password)
-            except Exception,e:
-                burp=False
-            if burp:
-                ssh.close()
-                self.result_pool[self.target+'_'+str(self.port)] = (self.now_user,password)
-                self.stop_signal = True
+            ssh.connect(self.target,self.port,self.now_user,password)
+            ssh.close()
+            self.result_pool[self.target+'_'+str(self.port)] = (self.now_user,password)
+            self.stop_signal = True
         except Exception,e:
             logging.error(e)
 
 if __name__ == '__main__':
     burp_test = ssh_burp()
-    burp_test.init_target('172.17.0.13',22)
-    burp_test.init_uspw(['root'],['root'])
+    burp_test.init_target('172.17.0.16',22)
+    burp_test.init_uspw(['1root'],['root'])
     burp_test.start_burp()
     burp_test.checkstate()
